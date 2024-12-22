48f5696977e14bcc0cec0d1ba7d63603a2922223
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-22 17:58:44 +0100

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,4 +1,24 @@
-document.getElementById("signup-form").addEventListener("submit", function(e) {
+
+// Import necessary Firebase modules
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAFyzUobF12L-QlVnklN-2uYKvhwqXdkyE",
+    authDomain: "veran350-b6c53.firebaseapp.com",
+    projectId: "veran350-b6c53",
+    storageBucket: "veran350-b6c53.firebasestorage.app",
+    messagingSenderId: "727775684680",
+    appId: "1:727775684680:web:80f53b3b9c4ec3b6b1107d",
+    measurementId: "G-EQE584VHBY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.getElementById("signup-form").addEventListener("submit", function (e) {
     e.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
