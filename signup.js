ddf05600d0f70bfcbb95ac99eab7b078ba7d74e8
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-22 17:54:07 +0100

    Create signup.js

---
+++ signup.js
@@ -0,0 +1,14 @@
+document.getElementById("signup-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    fetch('/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    })
+    .then(response => response.json())
+    .then(data => alert(data.message))
+    .catch(error => alert(error.message));
+});
